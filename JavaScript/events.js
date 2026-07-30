4d1e37d7a079561a28cafa249df6bb9b1a09bb84
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-07-30 22:25:09 +0400

    Countdown time day 1

--- JavaScript/events.js
+++ JavaScript/events.js
@@ -64,3 +64,62 @@ function tick_timer() {
 
     setTimeout(tick_timer, 100);
 }
+
+document.getElementById("btn-start").addEventListener("click", startCountdownTimer);
+function startCountdownTimer() {
+    let targetDateControl = document.getElementById("target-date");
+    let targetTimeControl = document.getElementById("target-time");
+    let btnStart = document.getElementById("btn-start");
+    if (btnStart.value === "Start") {
+        btnStart.value = "Stop";
+        targetDateControl.disabled = targetTimeControl.disabled = true;
+        tickCountdown();
+    }
+    else {
+        btnStart.value = "Start";
+        targetDateControl.disabled = targetTimeControl.disabled = false;
+       // clearTimeout(tickCountdown);
+    }
+}
+
+function tickCountdown()
+{
+    if (document.getElementById("btn-start").value === "Start") return;
+    let now = new Date();
+
+    let targetDate = document.getElementById("target-date").valueAsDate;
+    let targetTime = document.getElementById("target-time").valueAsDate;
+
+    //выравниваем часовой пояс:
+    targetDate.setHours(targetDate.getHours() + targetDate.getTimezoneOffset()/60);
+    targetTime.setHours(targetTime.getHours() + targetTime.getTimezoneOffset()/60);
+
+    //синхронизируем целевую дату и время:
+    targetTime.setFullYear(targetDate.getFullYear());
+    targetTime.setMonth(targetDate.getMonth());
+    targetTime.setDate(targetDate.getDate());
+
+    let timestamp = targetTime - now;
+
+    let duration = Math.trunc(timestamp / 1000);
+
+    document.getElementById("target-date-value").innerHTML = targetDate;
+    document.getElementById("target-time-value").innerHTML = targetTime;
+    document.getElementById("timezone").innerHTML = targetTime.getTimezoneOffset();
+    document.getElementById("duration").innerHTML = duration;
+    document.getElementById("timestamp").innerHTML = timestamp;
+
+    const SECONDS_PER_MINUTE = 60;
+    const SECONDS_PER_HOUR = 3600;
+    const SECONDS_PER_DAY = 86400;
+
+    let time_of_day = duration % SECONDS_PER_DAY;
+
+    document.getElementById("hours-unit").innerHTML = Math.trunc(time_of_day / SECONDS_PER_HOUR);
+    time_of_day = time_of_day % SECONDS_PER_HOUR;
+    document.getElementById("minutes-unit").innerHTML = Math.trunc(time_of_day / SECONDS_PER_MINUTE);
+    document.getElementById("seconds-unit").innerHTML = Math.trunc(time_of_day % SECONDS_PER_MINUTE);
+    
+
+    setTimeout(tickCountdown, 100);
+}
